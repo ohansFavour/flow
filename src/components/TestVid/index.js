85dc5a5460b7257647c76fd7e3c404a5228e1958
ohans favour <fohanekwu@gmail.com>
2021-01-10 03:33:30 +0100

    fix

--- src/components/TestVid/index.js
+++ src/components/TestVid/index.js
@@ -38,40 +38,79 @@ const TestVid = () => {
               </div>
             </div>
           );
-        } else {
+        } else if (y < 1000) {
           return (
-            <div className="section-two">
-              <Fade delay={500} bottom duration={4000}>
-                <img src={Barber} alt="" className="first" />
-              </Fade>
-              <Fade delay={1000} bottom duration={4000}>
-                <img src={Laptop} alt="" className="second" />
-              </Fade>
-              <Fade delay={1500} bottom duration={4000}>
-                <img src={Woman} alt="" className="third" />
-              </Fade>
-              <Fade delay={2000} bottom duration={4000}>
-                <img src={Electronic} alt="" className="fourth" />
-              </Fade>
+            <div className="section-two-cont">
+              <div className="section-two-container">
+                <div className="section-two">
+                  {/* <Fade delay={100} left duration={4000}> */}
+                  <img
+                    src={Barber}
+                    alt=""
+                    className="first"
+                    style={{
+                      left: `calc(((20%  * (${y} - 200)) / 800) - 20%)`,
+                      top: 0,
+                    }}
+                  />
+                  {/* </Fade> */}
+                  {/* <Fade delay={200} right duration={4000}> */}
+                  <img
+                    src={Laptop}
+                    alt=""
+                    className="second"
+                    style={{
+                      right: `calc(((20%  * (${y} - 200)) / 800) - 20%)`,
+                      top: 0,
+                    }}
+                  />
+                  {/* </Fade> */}
+                  {/* <Fade delay={300} left duration={4000}> */}
+                  <img
+                    src={Woman}
+                    alt=""
+                    className="third"
+                    style={{
+                      left: `calc(((20%  * (${y} - 200)) / 800) - 20%)`,
+                      bottom: 0,
+                    }}
+                  />
+                  {/* </Fade> */}
+                  {/* <Fade delay={400} right duration={4000}> */}
+                  <img
+                    src={Electronic}
+                    alt=""
+                    className="fourth"
+                    style={{
+                      right: `calc(((20%  * (${y} - 200)) / 800) - 20%)`,
+                      bottom: 0,
+                    }}
+                  />
+                  {/* </Fade> */}
 
-              <div className="test__main2">
-                <div>
-                  <h1>Kick-start your vocational career</h1>
-                  <p>
-                    We combine online and offline learning approaches to enable
-                    you acquire your desired vocational skill at your own pace.
-                  </p>
-                  <Tada duration={2000}>
+                  <div className="test__main2">
                     <div>
-                      <a href="/sign-up">
-                        <button>Sign Up</button>
-                      </a>
+                      <h1>Kick-start your vocational career</h1>
+                      <p>
+                        We combine online and offline learning approaches to
+                        enable you acquire your desired vocational skill at your
+                        own pace.
+                      </p>
+                      <Tada duration={2000}>
+                        <div>
+                          <a href="/sign-up">
+                            <button>Sign Up</button>
+                          </a>
+                        </div>
+                      </Tada>
                     </div>
-                  </Tada>
+                  </div>
                 </div>
               </div>
             </div>
           );
+        } else {
+          return <></>;
         }
       }}
     />
