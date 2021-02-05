e271ff2344e9632ab4cdc7950ce477647880e23d
Ohans Favour <fohanekwu@gmail.com>
2021-02-05 17:04:00 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { NotificationContainer } from "react-notifications";
 
 import "react-notifications/lib/notifications.css";
