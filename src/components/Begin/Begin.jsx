1ecb9ce7e2e4cb21e8e97e67e9b61acee6b0742f
Ohans Favour <fohanekwu@gmail.com>
2021-02-05 10:52:21 +0100

    routing fix

--- src/components/Begin/Begin.jsx
+++ src/components/Begin/Begin.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import "./Begin.css";
 
-const Begin = () => {
+const Begin = ({ history }) => {
   return (
     <div className="begin">
       <h3>Begin your one of a kind apprenticeship</h3>
@@ -14,4 +14,4 @@ const Begin = () => {
   );
 };
 
-export default Begin;
+export default withRouter(Begin);
