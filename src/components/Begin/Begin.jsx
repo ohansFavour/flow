1814ed97b0bfc556e6d82eefb54fe096a8849195
Ohans Favour <fohanekwu@gmail.com>
2021-02-05 13:29:54 +0100

    notification

--- src/components/Begin/Begin.jsx
+++ src/components/Begin/Begin.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./Begin.css";
 
-const Begin = ({ history }) => {
+const Begin = () => {
   return (
     <div className="begin">
       <h3>Begin your one of a kind apprenticeship</h3>
       <Link to="/signup">
-        <button>Sign up now</button>
+        <button>Get Started</button>
       </Link>
     </div>
   );
 };
 
-export default withRouter(Begin);
+export default Begin;
